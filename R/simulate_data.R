46cfca561e5152e00fdd0962da3e590de94d6e49
ohtanilson <suguru.ohtani.n@gmail.com>
2022-12-03 09:24:47 +0900

    added demand shifter

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -14,6 +14,8 @@ w <-
   rnorm(nk, mean = 3, sd = 1)
 r <-
   rnorm(nk, mean = 0, sd = 1)
+y <-
+  rnorm(nk, mean = 0, sd = 1)
 z <-
   rnorm(nk, mean = 10, sd = 1)
 ### instrumental variable ----
@@ -30,6 +32,8 @@ alpha0 <-
 alpha1 <-
   1
 alpha2 <-
+  1
+alpha3 <-
   1
 gamma0 <-
   1
@@ -52,11 +56,11 @@ epsilon_d <-
 # generate data ----
 # aggregate quantity ----
 Q <- 
-  (alpha0 - gamma0 - gamma2 * w - gamma3 * r + (epsilon_d - epsilon_c))/
+  (alpha0 + alpha3 * y - gamma0 - gamma2 * w - gamma3 * r + (epsilon_d - epsilon_c))/
   ((1 + theta) * (alpha1 + alpha2 * z) + gamma1)
 # aggregate price ----  
 P <- 
-  alpha0 - (alpha1 + alpha2 * z) * Q + epsilon_d
+  alpha0 - (alpha1 + alpha2 * z) * Q + alpha3 * y + epsilon_d
 
 group_id_k <-
   rep(
@@ -70,6 +74,7 @@ data <-
         P,
         w,
         r,
+        y,
         z,
         iv_w,
         iv_r) %>% 
@@ -80,10 +85,10 @@ plot(data$Q,
 # estimation ----
 ## demand ----
 demand_formula <-
-  "P ~ Q + Q:z|z + iv_w + iv_r"
+  "P ~ Q + Q:z + y|y + z + iv_w + iv_r"
 supply_formula <-
   paste("P ~ composite_z:Q + Q + w + r|",
-        "composite_z + w + r + iv_w + iv_r")
+        "composite_z + w + r + y")
 res_demand <-
   AER::ivreg(
     formula = demand_formula,
@@ -106,14 +111,18 @@ alpha0_hat <-
 alpha1_hat <-
   res_demand %>% 
   purrr::map_dbl(~coef(.)[2]) 
-alpha2_hat <-
+alpha3_hat <-
   res_demand %>% 
   purrr::map_dbl(~coef(.)[3]) 
+alpha2_hat <-
+  res_demand %>% 
+  purrr::map_dbl(~coef(.)[4]) 
 demand_hat <-
   cbind(
     alpha0_hat,
     alpha1_hat,
-    alpha2_hat
+    alpha2_hat,
+    alpha3_hat
   ) %>% 
   tibble::as_tibble() %>% 
   dplyr::mutate(
@@ -195,7 +204,7 @@ grouped_data <-
   ) %>% 
   dplyr::select(
     group_id_k,
-    alpha0_hat:alpha2_hat,
+    alpha0_hat:alpha3_hat,
     gamma0_hat:theta_hat
   )
 modelsummary::datasummary_skim(grouped_data)
