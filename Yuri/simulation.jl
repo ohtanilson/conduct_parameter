4f39e85064ad97a17be482739e90f4cf4fcbe55b
Yuri Matsumura <47453668+Yuri-Matsumura@users.noreply.github.com>
2022-12-03 19:20:58 -0600

    Add a file

--- Yuri/simulation.jl
+++ Yuri/simulation.jl
@@ -1,25 +1,27 @@
-
 using LinearAlgebra, Distributions
 using Statistics, Random, MultivariateStats
 using JuMP, Ipopt
 using DelimitedFiles, JLD, CSV, DataFrames
-using Plots, Combinatorics, Dates
+using Plots, Combinatorics, Dates, StatsPlots
 using Parameters: @unpack, @with_kw
 using Logging
+using Econometrics
 
+
+using GLM
 #---------------------------------------------------------------------------------------------------------
 
 market_parameters = @with_kw (
+    α_0 = 10,
     α_1 = 1,
     α_2 = 1,
+    α_3 = 1,
     γ_0 = 1,
     γ_1 = 1,
     γ_2 = 1,
     γ_3 = 1,
-    α_3 = 1,
-    α_0 = 10,
     θ = 0.5,
-    σ = 2,
+    σ = 1,
     T = 50,
     S = 1000,
 )
@@ -32,60 +34,175 @@ mutable struct market_data
     Z::Vector{Float64}
     IV::Matrix{Float64}
     P::Vector{Float64}
-    MC::Vector{Float64}
     Y::Vector{Float64}
 end
 
 
 
-function simulation_data(parameter)
+function simulation_data(parameter, s)
 
-    Random.seed!(1234)
-
-    @unpack α_0, α_3, α_1, α_2 ,γ_0 , γ_1 ,γ_2 ,γ_3, θ,σ ,T = parameter
+    @unpack α_0, α_1, α_2, α_3,γ_0 , γ_1 ,γ_2 ,γ_3, θ,σ ,T = parameter
     
-    Q = Float64[];N
+    Random.seed!(s * 12345)
+
+    Q = Float64[];
     P = Float64[];
-    MC = Float64[];
-    W  = Float64[];
-    R  = Float64[];
+    W = Float64[];
+    R = Float64[];
     Z = Float64[];
     Y = Float64[];
 
     IV = zeros(T,2);
-
     for t = 1:T
-        r_t = rand(Normal(0,1))
         z_t = rand(Normal(10,1))
         w_t = rand(Normal(3,1))
-        h_t = w_t + randn()
-        k_t = r_t + randn()
-
+        r_t = rand(Normal(0,1))
+        iv_w_t = w_t + randn()
+        iv_r_t = r_t + randn()
         Y_t = randn()
-        ε_c = rand(Normal(0,σ))
+
         ε_d = rand(Normal(0,σ))
+        ε_c = rand(Normal(0,σ))
 
-        Q_star = (α_0 + α_3 * Y_t - γ_0 - γ_2 * w_t - γ_3 *r_t)/ ((1+θ) * (α_1 + α_2 *z_t) + γ_1)
+        Q_t = (α_0 + α_3 * Y_t - γ_0 - γ_2 * w_t - γ_3 * r_t + ε_d - ε_c)/((1+θ) * (α_1 + α_2 *z_t) + γ_1)
 
-        Q_t = Q_star + (ε_d - ε_c)/((1+θ) * (α_1 + α_2 *z_t) + γ_1) # The aggregate quantity, Q
-
-        mc_t = γ_0 + γ_1 *Q_t + γ_2 * w_t + γ_3 * r_t + ε_c # Marginal cost
-
-        p_t = α_0 - (α_1 + α_2 * z_t )* Q_t + ε_d # The demand relation
+        p_t = α_0 - α_1 * Q_t  - α_2 * z_t * Q_t + α_3 * Y_t + ε_d 
 
         push!(Q, Q_t)
-        push!(MC,mc_t)
-        push!(P,p_t)
-        push!(W,w_t)
-        push!(R,r_t)
-        push!(Z,z_t)
-        push!(Y,Y_t)
-        IV[t,:] .= (h_t, k_t)
+        push!(P, p_t)
+        push!(W, w_t)
+        push!(R, r_t)
+        push!(Z, z_t)
+        push!(Y, Y_t)
+        IV[t,:] .= (iv_w_t, iv_r_t)
     end
 
-    data = market_data(Q, W, R, Z, IV, P, MC, Y)
+    data = market_data(Q, W, R, Z, IV, P, Y)
     return data
 end
+
+
+function TwoSLS_estimation(parameter, data)
+
+    Q = data.Q
+    W = data.W
+    R = data.R
+    Z = data.Z
+    P = data.P
+    IV = data.IV
+    Y  = data.Y
+
+    @unpack  T = parameter
+
+    Z_demand = hcat(ones(T), Z, IV, Y)
+    Q_hat = Z_demand * inv(Z_demand' * Z_demand) * Z_demand' *  Q
+    X_d = hcat(ones(T), -Q_hat, -Z .* Q_hat, Y)
+    α_hat = inv(X_d' * X_d) * (X_d' * P)
+
+
+    #=
+    data_1st = DataFrame(Q = Q, Z = Z, IV_w = IV[:,1], IV_r = IV[:,2], Y = Y)
+    iv_1st = lm(@formula(Q ~ Z + IV_w + IV_r + Y), data_1st)
+
+    Q_hat = predict(iv_1st)
+
+    data_2nd = DataFrame(P = P, Q_hat = -Q_hat, Q_Z = -Z .* Q_hat, Y = Y)
+    iv_2nd= lm(@formula(P ~ Q_hat + Q_Z + Y), data_2nd)
+    α_hat = coef(iv_2nd)
+    =#
+    
+    # Supply side estimation 
+
+    Z_supply = hcat(ones(T), W, R, (α_hat[2] .+ α_hat[3] .* Z), Y)
+    Q_hat = Z_supply * inv(Z_supply' * Z_supply) * Z_supply' *  Q
+    X_s = hcat(ones(T), Q_hat, W, R, (α_hat[2] .+ α_hat[3] .* Z) .* Q_hat)
+    γ_hat = inv(X_s' * X_s) * (X_s' * P)
+
+
+    θ_hat = γ_hat[end]
+
+    return α_hat, γ_hat[1:end-1], θ_hat
+end
+
+
+function simulation_2sls()
+
+    parameter = market_parameters()
+    α_est = Vector{Float64}[]
+    γ_est = Vector{Float64}[]
+    θ_est = Float64[]
+
+    for s = 1:1000
+        data_s = simulation_data(parameter, s);
+        α_est_s, γ_est_s, θ_est_s = TwoSLS_estimation(parameter, data_s)
+
+        push!(α_est, α_est_s)
+        push!(γ_est, γ_est_s)
+        push!(θ_est, θ_est_s)
+
+    end
+
+    α_est = reduce(vcat, α_est')
+    γ_est = reduce(vcat, γ_est')
+    θ_est = reduce(vcat, θ_est')
+
+    @show mean(α_est, dims = 1)
+    @show mean(γ_est, dims = 1)
+    @show mean(θ_est, dims = 1)
+end
+
+
+simulation_2sls()
+
+Results = []
+
+for sigma in [0.001, 0.5, 1, 2], t in [50, 100, 200, 1000]
+    
+
+    parameter = market_parameters(σ = sigma, T = t)
+    α_est = Vector{Float64}[]
+    γ_est = Vector{Float64}[]
+    θ_est = Float64[]
+
+    for s = 1:1000
+        data_s = simulation_data(parameter, s);
+        α_est_s, γ_est_s, θ_est_s = TwoSLS_estimation(parameter, data_s)
+
+        push!(α_est, α_est_s)
+        push!(γ_est, γ_est_s)
+        push!(θ_est, θ_est_s)
+    end
+
+    α_est = reduce(vcat, α_est')
+    γ_est = reduce(vcat, γ_est')
+    θ_est = reduce(vcat, θ_est')
+
+    result = DataFrame(
+    σ = sigma,
+    T = t,
+    α_0 = α_est[:,1],
+    α_1 = α_est[:,2],
+    α_2 = α_est[:,3],
+    α_3 = α_est[:,end],
+    γ_0 = γ_est[:,1],
+    γ_1 = γ_est[:,2],
+    γ_2 = γ_est[:,3],
+    γ_3 = γ_est[:,4],
+    θ = θ_est)
+
+    push!(Results, describe(result, :mean, :std, :min, :max, :median))
+
+end
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -94,48 +211,45 @@ function TwoSLS_estimation(parameter, data)
 
     @unpack T = parameter
 
-
     Q  = data.Q
     w  = data.W
     r  = data.R
     z  = data.Z
     iv = data.IV
-    P  = data.P
+    p  = data.P
+    Y  = data.Y
 
-    Z = []
-    Y = []
-    X = []
+    P = Vector{Float64}[]
+    Z = Matrix{Float64}[]
+    X = Matrix{Float64}[]
 
     for t = 1:T
-        
-        Z_ts = vcat(z[t], z[t] * Q[t], w[t], r[t], iv[t,:])
-        Z_td = vcat(z[t], z[t] * Q[t], w[t], r[t], iv[t,:])
+        Z_td = vcat(z[t], w[t], r[t], Y[t], iv[t,:])
+        Z_ts = vcat(z[t], w[t], r[t], Y[t])
 
-        Z_t = [Z_td zeros(length(Z_ts));  zeros(length(Z_td)) Z_ts]'
+        Z_t = [Z_td zeros(length(Z_td));  zeros(length(Z_ts)) Z_ts]'
 
-        X_td = vcat(1, -Q[t],-z[t].*Q[t])
+        X_td = vcat(1, -Q[t],-z[t].*Q[t], Y[t])
         X_ts = vcat(1, Q[t], w[t], r[t], z[t].*Q[t])
 
         X_t = [X_td zeros(length(X_td)); zeros(length(X_ts)) X_ts]'
 
-        push!(Y, vcat(P[t], P[t]))
+        push!(P, vcat(p[t], p[t]))
         push!(X, X_t)
         push!(Z, Z_t)
     end
 
-
     Z = reduce(vcat,(Z))
     X = reduce(vcat,(X))
-    Y = reshape(reduce(vcat,transpose.(Y))', (T * 2))
+    P = reshape(reduce(vcat,transpose.(P))', (T * 2))
 
-    β_hat = inv(X' * Z * inv(Z'Z) * Z' * X) * X' * Z * inv(Z'Z) * Z' * Y
+    β_hat = inv(X' * Z * inv(Z'Z) * Z' * X) * X' * Z * inv(Z'Z) * Z' * P
 
     α_0_hat, α_1_hat, α_2_hat  = β_hat[1], β_hat[2], β_hat[3]
     γ_0_hat, γ_2_hat, γ_3_hat = β_hat[4], β_hat[7],   β_hat[8]
 
     θ_hat = β_hat[6]/α_2_hat
     γ_1_hat = β_hat[5] - θ_hat * α_1_hat
-
 
     #return α_hat, γ_hat[1:end-1], θ_hat
     return β_hat, (α_0_hat, α_1_hat, α_2_hat ), (γ_0_hat, γ_1_hat, γ_2_hat, γ_3_hat), θ_hat
@@ -202,43 +316,6 @@ function ThreeSLS_estimation(parameter, data)
 
 end
 
-
-
-function Independent_TwoSLS_estimation(parameter, data)
-
-    Q = data.Q
-    W = data.W
-    R = data.R
-    Z = data.Z
-    P = data.P
-    IV = data.IV
-
-    @unpack  α_0, α_1, α_2, γ_0, γ_1, γ_2, γ_3, θ, T = parameter
-
-    Z_cost = hcat(W, R, Z, Z.* Q, IV)
-
-    Q_hat = Z_cost * (inv(Z_cost' * Z_cost) * (Z_cost' * Q))
-    
-    X_d = hcat(ones(T), -Q_hat, -Q_hat .* Z)
-
-    α_hat = inv(X_d' * X_d) * (X_d' * P);
-
-    X_s = hcat(ones(T), Q_hat, W, R, (α_hat[2] .+ α_hat[3] .* Z) .* Q_hat)
-
-    γ_hat =  inv(X_s' * X_s) * (X_s' * P); 
-
-    θ_hat = γ_hat[end]
-
-    return α_hat, γ_hat[1:end-1], θ_hat
-end
-
-
-α_hat, γ_hat, θ_hat = Independent_TwoSLS_estimation(parameter, data)
-
-# Two additional instruments are created by adding an additional random variable drawn from N(0,1) to w and to r
-# p = η + α*w + β*r +[θ * (α_1 + α_2 *z ) + γ] *Q + ε_c # Supply relationship
-parameter = market_parameters()
-data = simulation_data(parameter);
 β_hat, α_hat, γ_hat, θ_hat = TwoSLS_estimation(parameter, data)
 
 β_hat, α_hat, γ_hat, θ_hat = ThreeSLS_estimation(parameter, data)
@@ -247,6 +324,99 @@ data = simulation_data(parameter);
 
 
 
+
+function Tsls_model(s)
+
+    α = [1, 2, 3, 4]
+    γ = [1, 2, 3, 4]
+
+
+    X = Vector{Float64}[]
+    Z = Vector{Float64}[]
+    Y = Float64[]
+
+    Random.seed!(s * 100)
+    for t = 1:1000
+
+        ε = randn()
+        η = randn()
+
+        Z_t = vcat(1, randn(3))
+        X_exo = randn(2)
+        X_end = γ' * Z_t + η
+        X_t = vcat(1, X_exo, X_end)
+        Y_t = α' * X_t + ε
+
+        push!(Z, Z_t)
+        push!(Y, Y_t)
+        push!(X, X_t)
+    end
+
+    Z = reduce(vcat,(Z'))
+    X = reduce(vcat,(X'))
+    Y = reduce(vcat,(Y))
+
+    X_end_hat = Z * (inv(Z' * Z) * Z' * X[:,end])
+
+    X = hcat(X[:,1:end-1], X_end_hat)
+    α_IV = inv(X' * X) * (X' * Y)
+
+    #display(α_IV)
+
+    #α_IV = inv(X' * Z * inv(Z' * Z) * Z' * X) * (X' * Z * inv(Z' * Z) * Z' * Y) 
+
+    return α_IV
+end
+
+α_IV = Vector{Float64}[]
+for s = 1:1000
+    α_IV_s =  Tsls_model(s)
+
+    push!(α_IV, α_IV_s)
+end
+
+α_IV = reduce(vcat, α_IV')
+
+mean(α_IV, dims = 1)
+
+
+
+
+function ols_model()
+
+    α = [1, 2, 3, 4]
+
+    X = Vector{Float64}[]
+    Y = Float64[]
+
+    for t = 1:100
+        ε = randn()
+
+        X_t = vcat(1, randn(3))
+        Y_t = α' * X_t + ε
+
+        push!(Y, Y_t)
+        push!(X, X_t)
+    end
+
+    X = reduce(vcat,(X'))
+    Y = reduce(vcat,(Y))
+
+    α_ols = inv(X' * X) * (X' * Y)
+
+    return α_ols
+end
+
+α_ols = Vector{Float64}[]
+for s = 1:1000
+    α_ols_s =  ols_model()
+
+    push!(α_ols, α_ols_s)
+end
+
+α_ols = reduce(vcat, α_ols')
+
+mean(α_ols, dims = 1)
 
 #---------------------------------------------------------------------------------------------------------
 
@@ -305,8 +475,8 @@ function simulation_data_log(parameter)
         r_t = rand(Uniform(0,1))
         z_t = rand(Uniform(5,10))
         w_t = rand(Uniform(1,3))
-        h_t = w_t + randn()
-        k_t = r_t + randn()
+        iv_w_t = w_t + randn()
+        iv_r_t = r_t + randn()
 
 
         ε_c = rand(Normal(0,σ))
@@ -324,7 +494,7 @@ function simulation_data_log(parameter)
         push!(W,log(w_t))
         push!(R,log(r_t))
         push!(Z,z_t)
-        IV[t,:] .= (h_t, k_t)
+        IV[t,:] .= (iv_w_t, iv_r_t)
 
     end
 
---
+++ Yuri/simulation_with_R.jl
@@ -0,0 +1,94 @@
+using LinearAlgebra, Distributions
+using Statistics, Random, MultivariateStats
+using DataFrames, RData
+
+
+
+
+function TwoSLS_estimation_R(n, sigma,s)
+
+    data = load("../conduct_parameter/R/output/data_linear_linear_n_50_sigma_$sigma.rds")
+    data = data[(s-1)*n+1:s*n,:]
+
+    T = size(data, 1)
+
+    Q = data.Q
+    W = data.w
+    R = data.r
+    Z = data.z
+    P = data.P
+    iv_w = data.iv_w
+    iv_r = data.iv_r
+    Y = data.y
+    
+
+    Z_demand = hcat(ones(T), Z, iv_w, iv_r, Y)
+    Q_hat = Z_demand * inv(Z_demand' * Z_demand)* (Z_demand' * Q)
+    X_d = hcat(ones(T), -Q_hat, -Z .* Q_hat, Y)
+    α_hat = inv(X_d' * X_d) * (X_d' * P)
+
+
+    #=    
+    data_1st = DataFrame(Q = Q, Z = Z, IV_w = iv_w, IV_r = iv_r, Y = Y)
+    iv_1st = lm(@formula(Q ~ Z + IV_w + IV_r + Y), data_1st, dropcollinear=false)
+
+    Q_hat = predict(iv_1st)
+
+    data_2nd = DataFrame(P = P, Q_hat = -Q_hat, Q_Z = -Z .* Q_hat, Y = Y)
+    iv_2nd= lm(@formula(P ~ Q_hat + Q_Z + Y), data_2nd, dropcollinear=false)
+    α_hat_GLM = coef(iv_2nd)
+    =#
+
+    # Supply side estimation 
+
+    Z_supply = hcat(ones(T), W, R, (α_hat[2] .+ α_hat[3] .* Z), Y)
+    Q_hat = Z_supply * inv(Z_supply' * Z_supply) * Z_supply' *  Q
+    X_s = hcat(ones(T), Q_hat, W, R, (α_hat[2] .+ α_hat[3] .* Z) .* Q_hat)
+    γ_hat = inv(X_s' * X_s) * (X_s' * P)
+
+    θ_hat = γ_hat[end]
+
+    return α_hat, γ_hat[1:end-1], θ_hat
+
+end
+
+function simulation_2sls_R(n, sigma)
+
+
+    α_est = Vector{Float64}[]
+    γ_est = Vector{Float64}[]
+    θ_est = Float64[]
+
+    for s = 1:1000
+
+        α_est_s, γ_est_s, θ_est_s = TwoSLS_estimation_R(n,sigma, s)
+
+        push!(α_est, α_est_s)
+        push!(γ_est, γ_est_s)
+        push!(θ_est, θ_est_s)
+
+    end
+
+    α_est = reduce(vcat, α_est')
+    γ_est = reduce(vcat, γ_est')
+    θ_est = reduce(vcat, θ_est')
+
+    result = DataFrame(
+        σ = 1,
+        T = 50,
+        α_0 = α_est[:,1],
+        α_1 = α_est[:,2],
+        α_2 = α_est[:,3],
+        α_3 = α_est[:,end],
+        γ_0 = γ_est[:,1],
+        γ_1 = γ_est[:,2],
+        γ_2 = γ_est[:,3],
+        γ_3 = γ_est[:,4],
+        θ = θ_est
+    )
+
+    describe(result, :mean, :std, :min, :max, :median)
+
+end
+
+simulation_2sls_R(50, 1)
