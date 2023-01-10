59905fcf1198cec12ec1f859b4c219af8a975306
Yuri Matsumura <47453668+Yuri-Matsumura@users.noreply.github.com>
2023-01-09 18:50:09 -0600

    Update the code

--- code/03construct_figuretable_loglinear_loglinear_comparison.jl
+++ code/03construct_figuretable_loglinear_loglinear_comparison.jl
@@ -2,7 +2,7 @@ using LinearAlgebra, Distributions
 using Statistics, Random, MultivariateStats
 using JuMP, Ipopt
 using DelimitedFiles, JLD, CSV, DataFrames, RData
-using Plots, Combinatorics, Dates, StatsPlots
+using Plots, Combinatorics, Dates, StatsPlots, VegaLite
 using Parameters: @unpack, @with_kw
 
 
@@ -72,11 +72,7 @@ end
 
 #-----------------------------------------------------------------------------------------
 # Draw histograms consisting of the estimation reuslt of θ within [-2, 3]
-
-histogram_result_theta = []
-
 @unpack θ = parameter
-
 for t = [50, 100, 200, 1000], sigma =  [0.001, 0.5, 1, 2]
 
 
@@ -84,10 +80,7 @@ for t = [50, 100, 200, 1000], sigma =  [0.001, 0.5, 1, 2]
         sigma = Int64(sigma)
     end
     
-    histo_result = histogram(xlims = [-2, 3], title = " n = $t, σ = $sigma", legend = :topright, size = (800, 600))
-    #histo_result = density(xlims = [-2, 3],  title = " n = $t, σ = $sigma", legend = :topright, size = (800, 600))
-    
-    vline!([θ], label = "true value : θ = $θ")
+    histo_result = histogram(xlims = [-2, 3], title = " n = $t, σ = $sigma", legend = :topright, size = (800, 600))vline!([θ], label = "true value : θ = $θ")
 
     for estimation_method = estimation_methods
 
@@ -122,7 +115,6 @@ for t = [50, 100, 200, 1000], sigma =  [0.001, 0.5, 1, 2]
         end
 
         histogram!(histo_result, estimation_result, xlims = [-2, 3], xlabel = "θ",ylabel = "counts", label = label_title, fill = true, fillalpha = 0.5, bins = -2:0.1:3)
-        #density!(histo_result, estimation_result, xlims = [-2, 3], xlabel = "θ",ylabel = "counts", label = label_title, alpha = 0.5)
     end
 
     push!(histogram_result_theta, histo_result)
@@ -133,10 +125,6 @@ for t = [50, 100, 200, 1000], sigma =  [0.001, 0.5, 1, 2]
 
     savefig(histo_result, file_name)
 end
-   
-# Put all histograms in one plot
-#plot(histogram_result_theta...,  layout=(4,4), legend= true, size = (3000,3000))
-
 
 
 #-----------------------------------------------------------------------------------------
@@ -297,13 +285,260 @@ for t = [50, 100, 200, 1000], sigma =  [0.001, 0.5, 1, 2]
         vline!([θ], linestyle=:dash, label = "true θ")
         hline!([γ_0], linestyle=:dash, label = "true γ_0")
 
+
+
+    for estimation_method = estimation_methods
+
+        # Load the estimation result
+        filename_estimation = "_"*String(estimation_method[1])*"_"*String(estimation_method[2])*"_"*String(estimation_method[3])
+        filename_begin = "../conduct_parameter/output/parameter_hat_table_loglinear_loglinear_n_"
+        filename_end   = ".csv"
+        file_name = filename_begin*string(t)*"_sigma_"*string(sigma)*filename_estimation*filename_end
+        estimation_result = DataFrame(CSV.File(file_name))
+
+        if estimation_method[3] == :theta_constraint
+            label_title = "estimation with constraint"
+
+        else
+            label_title = "estimation without constraint"
+        end
+        # count the number of the estimation result out of [-2, 3]
+        scatter!([estimation_result.θ[1]], [estimation_result.γ_0[1]], lable = label_title)
+
+    end
+
+
+    display(plot_contour)
+
     filename_begin = "../conduct_parameter/figuretable/contour_loglinear_loglinear_n_"
     filename_end   = ".pdf"
     file_name = filename_begin*string(t)*"_sigma_"*string(sigma)*filename_end
 
-    savefig(plot_contour, file_name)
+    #savefig(plot_contour, file_name)
 end
 
 
 
 
+#-------------------------------------------------------------------------------
+
+# Plotting the estimation result of theta and gamma_0 for all simulations
+@unpack θ = parameter
+for t = [50, 100, 200, 1000], sigma =  [0.001, 0.5, 1, 2]
+
+
+    if sigma == 1 || sigma == 2
+        sigma = Int64(sigma)
+    end
+    
+    #histo_result = histogram(xlims = [-2, 3], title = " n = $t, σ = $sigma", legend = :topright, size = (800, 600))
+    #histo_result = density(xlims = [-2, 3],  title = " n = $t, σ = $sigma", legend = :topright, size = (800, 600))
+    
+    #vline!([θ], label = "true value : θ = $θ")
+
+    for estimation_method = estimation_methods
+
+        # Load the estimation result
+        filename_estimation = "_"*String(estimation_method[1])*"_"*String(estimation_method[2])*"_"*String(estimation_method[3])
+        filename_begin = "../conduct_parameter/output/parameter_hat_table_loglinear_loglinear_n_"
+        filename_end   = ".csv"
+        file_name = filename_begin*string(t)*"_sigma_"*string(sigma)*filename_estimation*filename_end
+        estimation_result = DataFrame(CSV.File(file_name))
+
+        # count the number of the estimation result out of [-2, 3]
+        estimation_result  = dropmissing(estimation_result, :θ);
+        estimation_status = deepcopy(estimation_result.status)
+
+        #=
+        estimation_status = deepcopy(estimation_result.status)
+
+        for i = eachindex(estimation_status)
+            if estimation_status[i] == 4
+                estimation_status[i] = 1
+            else
+                estimation_status[i] = 0
+            end
+        end
+
+        =#
+        constraint = estimation_method[3]
+
+        estimation_plot = plot(scatter(estimation_result.θ, estimation_result.γ_0,  group = estimation_status, alpha = 0.7), xlabel = "θ", ylable = "γ_0", size = (500, 400), title = " n = $t, σ = $sigma, $constraint")
+
+        display(estimation_plot)
+
+        #histogram!(histo_result, estimation_result, xlims = [-2, 3], xlabel = "θ",ylabel = "counts", label = label_title, fill = true, fillalpha = 0.5, bins = -2:0.1:3)
+    end
+
+    #push!(histogram_result_theta, histo_result)
+    filename_begin = "../conduct_parameter/figuretable/histogram_loglinear_loglinear_n_"
+    filename_end   = ".pdf"
+    file_name = filename_begin*string(t)*"_sigma_"*string(sigma)*filename_end
+
+    #savefig(histo_result, file_name)
+end
+
+
+
+
+#-----------------------------------------------------------------------------------------
+
+function value_GMM(parameter, data, estimation_result)
+    
+
+    @unpack α_0, α_1, α_2, α_3, γ_0, γ_1 ,γ_2 ,γ_3, θ, σ ,T = parameter
+
+    γ = [γ_0 , γ_1 ,γ_2 ,γ_3]
+    α = [α_0, α_1, α_2, α_3]
+
+    Q  = data.logQ
+    w  = data.w
+    r  = data.r
+    z  = data.z
+    iv_w = data.iv_w
+    iv_r = data.iv_r
+    p  = data.logP
+    y  = data.y
+
+    iv = hcat(iv_w, iv_r)
+
+    X_d = []
+    X_s = []
+    X   = []
+    Z   = []
+    Z_d = []
+    Z_s = []
+    P   = []
+
+    for t = 1:T
+
+        Z_dt = vcat(1, z[t], iv[t,:], y[t])
+        Z_st = vcat(1, z[t], log(w[t]), log(r[t]), y[t])
+        Z_t = [Z_dt zeros(length(Z_dt));  zeros(length(Z_st)) Z_st]'
+
+        X_dt = vcat(1, -Q[t], -z[t].*Q[t], y[t])
+        X_st = vcat(1, Q[t], log(w[t]), log(r[t]), z[t])
+        X_t  = [X_dt zeros(length(X_dt));  zeros(length(X_st)) X_st]'
+
+        push!(P, p[t])
+        push!(X_d, X_dt')
+        push!(X_s, X_st')
+        push!(X, X_t)
+        push!(Z, Z_t)
+        push!(Z_d, Z_dt')
+        push!(Z_s, Z_st')
+    end
+
+    Z   = reduce(vcat,(Z))
+    X   = reduce(vcat,(X))
+    X_d = reduce(vcat,(X_d))
+    X_s = reduce(vcat,(X_s))
+    Z_d = reduce(vcat,(Z_d))
+    Z_s = reduce(vcat,(Z_s))
+
+    L = size(Z, 2)
+    L_d = size(Z_d,2)
+    L_s = size(Z_s,2)
+    K_s = size(X_s, 2)
+    K_d = size(X_d, 2)
+
+    Ω = inv(Z_s' * Z_s)/T
+    θ_hat = estimation_result.θ
+    γ_hat = estimation_result.γ_0
+
+
+    
+    if 1 <= sum(1 .- θ_hat .*(α_1 .+ α_2 .* X_s[:, end]) .<= 0)
+        return missing, missing, missing
+    else
+        
+        r_true = P .- γ_0 .-sum(γ[k] .* X_s[:,k] for k = 2:K_s-1) + log.(1 .- θ .*(α_1 .+ α_2 .* X_s[:, end]) ) 
+        r_estimated = P .- γ_hat .-sum(γ[k] .* X_s[:,k] for k = 2:K_s-1) + log.(1 .- θ_hat .*(α_1 .+ α_2 .* X_s[:, end]) ) 
+
+        g_true      = sum(Z_s[t,:] .* r_true[t] for t = 1:T)
+        g_estimated = sum(Z_s[t,:] .* r_estimated[t] for t = 1:T)
+
+        gmm_value_true = sum( g_true[l] *Ω[l,k] * g_true[k] for l = 1:L_s, k = 1:L_s)
+        gmm_value_estimated = sum( g_estimated[l] *Ω[l,k] * g_estimated[k] for l = 1:L_s, k = 1:L_s)
+
+
+        difference_theta = abs(θ_hat - θ)
+        difference_gamma = abs(γ_hat - γ_0)
+        difference_gmm_value = abs(gmm_value_true - gmm_value_estimated)
+
+        return difference_theta, difference_gamma, difference_gmm_value
+    end
+end
+
+
+# Check the contour figure for global range
+for t = [50, 100, 200, 1000], sigma =  [0.001, 0.5, 1, 2]
+    @unpack θ, γ_0, S = parameter
+
+    if sigma == 1 || sigma == 2
+        sigma = Int64(sigma)
+    end
+    
+    # Load the simulation data from the rds files
+    filename_begin = "../conduct_parameter/output/data_loglinear_loglinear_n_"
+    filename_end   = ".rds"
+
+    if sigma == 1 || sigma == 2
+        sigma = Int64(sigma)
+    end
+
+    filename = filename_begin*string(t)*"_sigma_"*string(sigma)*filename_end
+    data = load(filename)
+    data = DataFrames.sort(data, [:group_id_k])
+
+    for estimation_method = estimation_methods
+
+        difference_theta = Union{Missing, Float64}[]
+        difference_gamma = Union{Missing, Float64}[]
+        difference_gmm_value = Union{Missing, Float64}[]
+
+        # Load the estimation result
+        filename_estimation = "_"*String(estimation_method[1])*"_"*String(estimation_method[2])*"_"*String(estimation_method[3])
+        filename_begin = "../conduct_parameter/output/parameter_hat_table_loglinear_loglinear_n_"
+        filename_end   = ".csv"
+        file_name = filename_begin*string(t)*"_sigma_"*string(sigma)*filename_estimation*filename_end
+        estimation_result = DataFrame(CSV.File(file_name))
+
+        if estimation_method[3] == :theta_constraint
+            label_title = "estimation with constraint"
+
+        else
+            label_title = "estimation without constraint"
+        end
+
+        data = dropmissing(data, :logQ)
+        estimation_result = dropmissing(estimation_result, :θ);
+
+        number_sample = size(estimation_result,1)
+
+        for s = 1:number_sample
+
+            data_s = data[(s-1)*t+1:s*t,:]
+            estimation_result_s = estimation_result[s,:]
+            diff_theta, diff_gamma, diff_gmm_value = value_GMM(parameter, data_s, estimation_result_s);
+
+            push!(difference_theta, diff_theta)
+            push!(difference_gamma, diff_gamma)
+            push!(difference_gmm_value, diff_gmm_value)
+        end
+
+        constraint = estimation_method[3]
+        
+        df = DataFrame(theta = difference_theta, gamma = difference_gamma, gmm_value = difference_gmm_value)
+        df |> @vlplot(:circle, x=:theta, y=:gamma, color = {:gmm_value, scale={scheme=:plasma}}, 
+        title = "n = $t, σ = $sigma, $constraint")|> display
+    end
+
+
+
+    filename_begin = "../conduct_parameter/figuretable/contour_loglinear_loglinear_n_"
+    filename_end   = ".pdf"
+    file_name = filename_begin*string(t)*"_sigma_"*string(sigma)*filename_end
+
+    #savefig(plot_contour, file_name)
+end
