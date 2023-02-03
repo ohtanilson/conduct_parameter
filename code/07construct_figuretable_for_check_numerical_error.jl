57cd3ebdc80351de7f34b3c62a5bb85ba068a373
Yuri Matsumura <47453668+Yuri-Matsumura@users.noreply.github.com>
2023-02-02 22:34:41 -0600

    Update julia

--- code/07construct_figuretable_for_check_numerical_error.jl
+++ code/07construct_figuretable_for_check_numerical_error.jl
@@ -10,9 +10,9 @@ tol_list = [:tight, :loose];
 # Histograms for the simulation results that use different combinations of tolerances and starting values
 #---------------------------------------------------------------------------------------------------------
 for estimation_method = estimation_methods
-    for t = [50, 100, 200, 1000], sigma = [0.001, 0.5, 1, 2]
-        for starting_value_used = starting_value_list
-            for tol_list_used = tol_list
+    for starting_value_used = starting_value_list
+        for tol_list_used = tol_list
+            for t = [50, 100, 200, 1000], sigma = [0.001, 0.5, 1, 2]
                 @unpack θ = parameter
 
                 if sigma == 1 || sigma == 2
@@ -23,19 +23,19 @@ for estimation_method = estimation_methods
                 if starting_value_used == :true && tol_list_used == :tight
                     filename_begin = "../conduct_parameter/output/tight/parameter_hat_table_loglinear_loglinear_n_"
                     filename_end   = "_true_start.csv"
-                    title = "true start, tight, n = $t, σ = $sigma"
+                    histo_title = "true start, tight, n = $t, σ = $sigma"
                 elseif starting_value_used == :random && tol_list_used == :tight
                     filename_begin = "../conduct_parameter/output/tight/parameter_hat_table_loglinear_loglinear_n_"
                     filename_end   = "_random_start.csv"
-                    title = "random start, tight, n = $t, σ = $sigma"
+                    histo_title = "random start, tight, n = $t, σ = $sigma"
                 elseif starting_value_used == :true && tol_list_used == :loose
                     filename_begin = "../conduct_parameter/output/loose/parameter_hat_table_loglinear_loglinear_n_"
                     filename_end   = "_true_start.csv"
-                    title = "true start, loose, n = $t, σ = $sigma"
+                    histo_title = "true start, loose, n = $t, σ = $sigma"
                 elseif starting_value_used == :random && tol_list_used == :loose
                     filename_begin = "../conduct_parameter/output/loose/parameter_hat_table_loglinear_loglinear_n_"
                     filename_end   = "_random_start.csv"
-                    title = "random start, loose, n = $t, σ = $sigma"
+                    histo_title = "random start, loose, n = $t, σ = $sigma"
                 end
 
                 filename_estimation = "_"*String(estimation_method[1])*"_"*String(estimation_method[2])*"_"*String(estimation_method[3])
@@ -44,6 +44,8 @@ for estimation_method = estimation_methods
         
                 rate_convergence = count( x -> (x <= 7), dropmissing(estimation_result, :status).status)/length(dropmissing(estimation_result, :status).status)
         
+                println("rate of convergence is ", round(rate_convergence * 100, digits = 1), " when t = $t, σ = $sigma, $starting_value_used, and $tol_list_used")
+
                 # count the number of the estimation result out of [0, 1]
                 estimation_result = dropmissing(estimation_result, :θ);
                 estimation_result = filter(row -> (row.status_indicator == 1), estimation_result)
@@ -53,14 +55,13 @@ for estimation_method = estimation_methods
                 rate_out_range = round(number_out_range/number_non_missing * 100, digits = 3)
                 rate_out_range = "$rate_out_range %"
                 
-                estimation_result = filter(x -> (-10^7<= x <= 10^3), estimation_result.θ)
+                estimation_result = filter(x -> (-2 <= x <= 3), estimation_result.θ)
         
                 label_title = "no constraint ($rate_out_range are out of [0, 1])"
-        
 
-                histo_result = Plots.histogram(xlims = [0, 1], title = title, legend = :topright, size = (800, 600))
+                histo_result = Plots.histogram(xlims = [0, 1], title = histo_title, legend = :topright, size = (800, 600))
                 Plots.vline!(histo_result, [θ], label = "true value : θ = $θ")
-                Plots.histogram!(histo_result, estimation_result, xlims = [-10^7, 10^3], xlabel = "θ",ylabel = "counts", label = label_title, fill = true, fillalpha = 0.5, bins = -10^7:1000:10^3)
+                Plots.histogram!(histo_result, estimation_result, xlims = [-2, 3], xlabel = "θ",ylabel = "counts", label = label_title, fill = true, fillalpha = 0.5, bins = -2:0.1:3)
         
                 if starting_value_used == :true && tol_list_used == :tight
                     filename_begin = "../conduct_parameter/figuretable/tight/histogram_loglinear_loglinear_n_"
