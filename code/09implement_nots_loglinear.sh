ab72c9eb659b8fcd6c88cdf609da59b1fec0d5b0
Yuri Matsumura <47453668+Yuri-Matsumura@users.noreply.github.com>
2023-05-02 08:41:31 +0900

    Update slrm file

--- code/09implement_nots_loglinear.sh
+++ code/09implement_nots_loglinear.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=20               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1         # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=5G          # memory per cpu-core (4G is default)
-#SBATCH --time=24:00:00           # total run time limit (HH:MM:SS)
+#SBATCH --time=02:00:00           # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=All           # send email when job begins and ends
 #SBATCH --mail-user=ym23@rice.edu
 #SBATCH --output=conduct_parameter_loglinear.out
@@ -12,4 +12,4 @@
 module purge
 module load Julia/1.8.2
 
-julia "../conduct_parameter/code/08run_simulation_loglinear.jl"
+julia "../conduct_parameter/code/08_1run_simulation_loglinear.jl"
