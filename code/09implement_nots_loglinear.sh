7e216d396dd4f647180c9eaaee4b6754bf76bc69
Yuri Matsumura <47453668+Yuri-Matsumura@users.noreply.github.com>
2023-07-17 19:34:15 +0900

    Update sh file

--- code/09implement_nots_loglinear.sh
+++ code/09implement_nots_loglinear.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=20               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1         # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=5G          # memory per cpu-core (4G is default)
-#SBATCH --time=01:00:00           # total run time limit (HH:MM:SS)
+#SBATCH --time=24:00:00           # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=All           # send email when job begins and ends
 #SBATCH --mail-user=ym23@rice.edu,so19@rice.edu
 #SBATCH --output=conduct_parameter_loglinear.out
